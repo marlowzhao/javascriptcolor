6fefabd580602c756747b9459e5553bfa5f5f97f
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-08-10 17:59:55 +0200

    toggle button

--- test.js
+++ test.js
@@ -1,20 +1,49 @@
-// const convertRGBToHex =(r,g,b)=>{
-//  const firstPair =("0" + r.toString(16)).slice(-2);
-//  const secondPair =("0" + g.toString(16)).slice(-2);
-//  const thirdPair =("0" + b.toString(16)).slice(-2);
+const isValidHex =(hex)=> {
+  if(!hex) return false;
 
-//  const hex = "#"+ firstPair+secondPair+thirdPair;
-//  return hex
-// }
+  const strippedHex = hex.replace('#', '');
+  return strippedHex.length===3 || strippedHex.length===6;
+}
 
-// console.log(convertRGBToHex(0,255,255))
+const converterHexToRGB=(hex)=>{
+  if(!isValidHex(hex)) return null;
+  let strippedHex = hex.replace('#', '');
 
-const convertRGBToHex =(r,g,b)=>{
-  const array = [r,g,b]
-  let text = [];
-    for (let i=0; i<array.length; i++){
-    text += ("0" + array[i].toString(16)).slice(-2);
-    }
-    return "#" + text
- }
- console.log(convertRGBToHex(0,255,255))
+  if(strippedHex.length===3){
+    strippedHex = strippedHex[0]+strippedHex[0]+strippedHex[1]+strippedHex[1]+strippedHex[2]+strippedHex[2];
+  }
+
+  const r = parseInt(strippedHex.substring(0,2),16);
+  const g = parseInt(strippedHex.substring(2,4),16);
+  const b = parseInt(strippedHex.substring(4,6),16);
+  return{r,g,b}
+}
+
+
+  const convertRGBToHex =(r,g,b)=>{
+    const array = [r,g,b]
+    let text = [];
+      for (let i=0; i<array.length; i++){
+      text += ("0" + array[i].toString(16)).slice(-2);
+      }
+      return "#" + text
+   }
+
+   const alterColor=(hex,percentage)=>{
+    //destructuring
+    const {r,g,b}=converterHexToRGB(hex);
+    const amount = Math.floor((percentage/100)*255)
+    const newR = r + amount;
+    const newG = g + amount;
+    const newB = b + amount;
+    return convertRGBToHex(newR,newG,newB);
+   }
+
+   const numberBtw0255=(hex, amount)=>{
+    const newHex = hex + amount;
+    if (newHex >255) return 255;
+    if (newHex <0) return 0;
+    return newHex
+  }
+  
+   console.log(alterColor('000',10));
